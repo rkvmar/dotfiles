96ac1ae6cb3844f906f21318853bf4ce9d809a9d
Ravi <108709054+rkvmar@users.noreply.github.com>
2025-02-15 15:04:36 -0800

    update sh to work

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@
 
 function install-brew-dependencies {
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo >> ~/.zprofile
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
   brew install nvim
   brew install git
@@ -14,7 +12,7 @@ function install-brew-dependencies {
 function zsh-setup {
   [ -f ~/.zshrc ] && mv ~/.zshrc{,.bak}
   [ ! -d ~/powerlevel10k ] && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-  echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme \n source ~/dotfiles/terminal/p10k.zsh' >>~/.zshrc
+  echo 'export PATH="/opt/homebrew/bin:$PATH" \n source ~/powerlevel10k/powerlevel10k.zsh-theme \n source ~/dotfiles/terminal/p10k.zsh' >>~/.zshrc
   # echo 'source ~/dotfiles/terminal/p10k.zsh' >> ~/.zshrc
   # source ~/.zshrc
   # exec zsh
