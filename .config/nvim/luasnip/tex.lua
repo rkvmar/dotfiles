e1f1556bad6d2388d47da57d6635e15e6523ce4c
Ravi <r2a3v5i7.kumar@gmail.com>
2025-09-04 07:06:18 -0700

    fix things

--- .config/nvim/luasnip/tex.lua
+++ .config/nvim/luasnip/tex.lua
@@ -1,26 +1,67 @@
 return {
-  s({ trig = ";a", snippetType = "autosnippet" }, {
-    t("\\alpha"),
-  }),
-  s({ trig = ";b", snippetType = "autosnippet" }, {
-    t("\\beta"),
-  }),
-  s({ trig = ";g", snippetType = "autosnippet" }, {
-    t("\\gamma"),
-  }),
+    s({ trig = ";a", snippetType = "autosnippet" }, {
+        t("\\alpha"),
+    }),
+    s({ trig = ";b", snippetType = "autosnippet" }, {
+        t("\\beta"),
+    }),
+    s({ trig = ";g", snippetType = "autosnippet" }, {
+        t("\\gamma"),
+    }),
 
-  s(
-    { trig = "eq", dscr = "A LaTeX equation environment" },
-    fmt( -- The snippet code actually looks like the equation environment it produces.
-      [[
+    s(
+        { trig = "eq", dscr = "A LaTeX equation environment" },
+        fmt(
+            [[
       \begin{equation}
           <>
       \end{equation}
     ]],
-      -- The insert node is placed in the <> angle brackets
-      { i(1) },
-      -- This is where I specify that angle brackets are used as node positions.
-      { delimiters = "<>" }
-    )
-  ),
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s(
+        { trig = ";init", dscr = "init latex", snippetType = "autosnippet" },
+        fmt(
+            [[
+            \documentclass{article}
+            \usepackage{geometry}
+            \geometry{margin=0.7in}
+            \parskip = \baselineskip
+            \setlength\parindent{0pt}
+            \usepackage{graphicx}
+            \usepackage{amsmath}
+            \usepackage{tikz}
+            \usetikzlibrary{regular-polygon}
+            \usepackage{float}
+
+            \title{}
+            \author{}
+            \date{}
+
+            \begin{document}
+            \maketitle
+            <>
+
+            \end{document}
+    ]],
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s(
+        { trig = ";img", dscr = "image", snippetType = "autosnippet" },
+        fmt(
+            [[
+            \begin{figure}
+              \includegraphics[width=\linewidth]{<>}
+              \caption{}
+              \label{}
+            \end{figure}
+    ]],
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
 }
