d14714e4c728db6f9b000c6660ebc3a6eeac94ba
Ravi <r2a3v5i7.kumar@gmail.com>
2025-12-13 11:23:47 -0800

    new things

--- .config/nvim/luasnip/all.lua
+++ .config/nvim/luasnip/all.lua
@@ -1,1 +1,18 @@
-
+return {
+  s(
+    { trig = ";kanji", dscr = "Kanji template for my kanji website" },
+    fmt(
+      [[
+      {
+        kanji: "<>",
+        meaning: "",
+        reading: "",
+        chapter: "",
+        subchapter: "",
+      },
+      ]],
+      { i(1) },
+      { delimiters = "<>" }
+    )
+  ),
+}
